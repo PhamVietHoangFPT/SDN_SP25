a73f048534c3a17062e139855f1128bdeb4610e8
PhamVietHoang <hoangpvse183123@fpt.edu.vn>
2025-02-28 13:30:59 +0700

    Fix auth

--- BE/routes/manageProductRoutes.js
+++ BE/routes/manageProductRoutes.js
@@ -3,25 +3,17 @@ const manageProductController = require("../Controller/manageProductController")
 const manageProductRouter = express.Router();
 
 const checkRole = require("../middlewares/authMiddleware");
+const manager = require("../constant/constant").manager
 
 manageProductRouter
-  .route("/", checkRole("Manage"))
-  .get(manageProductController.getAllProduct);
-
+  .route("/")
+  .get(checkRole(manager), manageProductController.getAllProduct)
+  .post(checkRole(manager), manageProductController.addProduct)
+  
 manageProductRouter
-  .route("/:id", checkRole("Manage"))
-  .get(manageProductController.getProductById);
-
-manageProductRouter
-  .route("/", checkRole("Manage"))
-  .post(manageProductController.addProduct);
-
-manageProductRouter
-  .route("/:id", checkRole("Manage"))
-  .put(manageProductController.updateProduct);
-
-manageProductRouter
-  .route("/:id", checkRole("Manage"))
-  .delete(manageProductController.deleteProduct);
+  .route("/:id")
+  .get(checkRole(manager), manageProductController.getProductById)
+  .put(checkRole(manager), manageProductController.updateProduct)
+  .delete(checkRole(manager), manageProductController.deleteProduct)
 
 module.exports = manageProductRouter;
