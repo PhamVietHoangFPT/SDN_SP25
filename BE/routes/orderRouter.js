c1d1c1193603897f455a8013300e589b403bf976
hainguyen130103 <hai263672@gmail.com>
2025-03-08 15:43:16 +0700

    manage Order user

--- BE/routes/orderRouter.js
+++ BE/routes/orderRouter.js
@@ -1,7 +1,17 @@
-const express = require('express')
-const orderController = require('../Controller/orderController.js')
-const orderController = express.Router()
+const express = require("express");
+const { checkRole } = require("../middlewares/authMiddleware");
+const orderController = require("../Controller/orderController.js");
+const orderRouter = express.Router();
 
-orderController.route('/')
-  .get(orderController.getAllOrder)
-  .post(orderController.addOrder)
+orderRouter
+  .route("/")
+  .get(checkRole("Customer"), orderController.getAllOrder)
+  .post(checkRole("Customer"), orderController.addOrder);
+
+orderRouter
+  .route("/:id")
+  .get(checkRole("Customer"), orderController.getOrderByID)
+  .put(checkRole("Customer"), orderController.updateOrderByID)
+  .delete(checkRole("Customer"), orderController.deleteOrder);
+
+module.exports = orderRouter;
